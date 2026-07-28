eb77d822de78b6711a83b769c60450c02ffe3129
charavelg <guillaume.charavel@inria.fr>
2026-07-28 17:17:44 +0200

    Add soft-dtw benchmarks

--- test_benchmark_dtw.py
+++ test_benchmark_dtw.py
@@ -10,7 +10,7 @@ from tslearn.barycenters import dtw_barycenter_averaging
 from tslearn.barycenters import dtw_barycenter_averaging_petitjean
 
 
-@pytest.mark.parametrize("sz", [10, 100, 1000])
+@pytest.mark.parametrize("sz", [100, 1000, 10000])
 def test_dtw_numpy(benchmark, sz):
     X = np.random.rand(sz, 2)
     benchmark(dtw, X, X)
@@ -18,23 +18,23 @@ def test_dtw_numpy(benchmark, sz):
 
 @pytest.mark.parametrize("sz", [10, 100, 1000])
 def test_dtw_torch_cpu(benchmark, sz):
-    X = torch.rand((10, 2))
+    X = torch.rand((sz, 2))
     benchmark(dtw, X, X)
 
 
-@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1), (100, 1000, -1)])
+@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1), (10, 1000, 1), (10, 1000, -1)])
 def test_cdist_dtw_numpy(benchmark, n_ts, sz, n_jobs):
     X = np.random.rand(n_ts, sz, 2)
     benchmark(cdist_dtw, X, X, n_jobs=n_jobs)
 
 
-@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1)])
+@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1), (10, 100, -1)])
 def test_cdist_dtw_torch_cpu(benchmark, n_ts, sz, n_jobs):
     X = torch.rand((n_ts, sz, 2))
     benchmark(cdist_dtw, X, X, n_jobs=n_jobs)
 
 
-@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1), (100, 1000, -1)])
+@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1), (10, 1000, -1)])
 def test_dtw_barycenter_petitjean(benchmark, n_ts, sz, n_jobs):
     rng = np.random.default_rng(0)
     X = rng.random((n_ts, sz, 2))
@@ -44,7 +44,7 @@ def test_dtw_barycenter_petitjean(benchmark, n_ts, sz, n_jobs):
         benchmark(dtw_barycenter_averaging_petitjean, X, max_iter=1)
 
 
-@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1), (100, 1000, -1)])
+@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1), (10, 1000, -1)])
 def test_dtw_barycenter(benchmark, n_ts, sz, n_jobs):
     rng = np.random.default_rng(0)
     X = rng.random((n_ts, sz, 2))
---
+++ test_benchmark_soft_dtw.py
@@ -0,0 +1,54 @@
+import inspect
+
+import numpy as np
+import pytest
+import torch
+
+from tslearn.metrics import soft_dtw
+from tslearn.metrics import cdist_soft_dtw
+from tslearn.barycenters import softdtw_barycenter
+
+
+@pytest.mark.parametrize("sz", [100, 1000, 10000])
+def test_soft_dtw_numpy(benchmark, sz):
+    X = np.random.rand(sz, 2)
+    benchmark(soft_dtw, X, X)
+
+
+@pytest.mark.parametrize("sz", [100, 1000])
+def test_soft_dtw_torch_cpu(benchmark, sz):
+    X = torch.rand((sz, 2))
+    kwargs = {}
+    if 'compute_with_backend' in inspect.signature(soft_dtw).parameters:
+        kwargs.update({'compute_with_backend': True})
+    benchmark(soft_dtw, X, X, **kwargs)
+
+
+@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1), (10, 1000, 1), (10, 1000, -1)])
+def test_cdist_soft_dtw_numpy(benchmark, n_ts, sz, n_jobs):
+    X = np.random.rand(n_ts, sz, 2)
+    kwargs = {}
+    if 'n_jobs' in inspect.signature(cdist_soft_dtw).parameters:
+        kwargs.update({'n_jobs': n_jobs})
+    benchmark(cdist_soft_dtw, X, X, **kwargs)
+
+
+@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1), (10, 100, -1)])
+def test_cdist_soft_dtw_torch_cpu(benchmark, n_ts, sz, n_jobs):
+    X = torch.rand((n_ts, sz, 2))
+    kwargs = {}
+    if 'compute_with_backend' in inspect.signature(soft_dtw).parameters:
+        kwargs.update({'compute_with_backend': True})
+    if 'n_jobs' in inspect.signature(cdist_soft_dtw).parameters:
+        kwargs.update({'n_jobs': n_jobs})
+    benchmark(cdist_soft_dtw, X, X, **kwargs)
+
+
+@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1), (10, 1000, -1)])
+def test_soft_dtw_barycenter(benchmark, n_ts, sz, n_jobs):
+    rng = np.random.default_rng(0)
+    X = rng.random((n_ts, sz, 2))
+    if 'n_jobs' in inspect.signature(softdtw_barycenter).parameters:
+        benchmark(softdtw_barycenter, X, max_iter=1, n_jobs=n_jobs)
+    else:
+        benchmark(softdtw_barycenter, X, max_iter=1)
