58d220eab4f301211d6199d35005f1c345fb876b
charavelg <guillaume.charavel@inria.fr>
2026-07-30 11:14:28 +0200

    Update benchmark

--- test_benchmark_dtw.py
+++ test_benchmark_dtw.py
@@ -9,8 +9,10 @@ from tslearn.metrics import cdist_dtw
 from tslearn.barycenters import dtw_barycenter_averaging_petitjean
 
 
-@pytest.mark.parametrize("sz", [100, 1000, 2000, 10000, 20000])
+@pytest.mark.parametrize("sz", [100, 1000, 2000, 4000, 8000])
 def test_dtw_numpy(benchmark, sz):
+    # Mem: sz * 64 * 3 (inputs duplicated twice) + sz * sz * 8 (mask) + sz *sz * 64 (acc matrix)
+    # Care for swap, check runner mem
     X = np.random.rand(sz, 2)
     benchmark(dtw, X, X)
 
@@ -21,19 +23,22 @@ def test_dtw_torch_cpu(benchmark, sz):
     benchmark(dtw, X, X)
 
 
-@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1), (10, 1000, 1), (10, 1000, -1), (50, 5000, -1)])
+@pytest.mark.parametrize("n_ts, sz, n_jobs", [(8, 1000, 1), (8, 2000, 1), (8, 2000, -1), (16, 2000, -1)])
 def test_cdist_dtw_numpy(benchmark, n_ts, sz, n_jobs):
+    # Mem: dtw_mem * n_jobs_effective + n_ts * n_ts *64
+    # Care for swap, check runner mem
     X = np.random.rand(n_ts, sz, 2)
     benchmark(cdist_dtw, X, X, n_jobs=n_jobs)
 
 
-@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1), (10, 100, -1)])
+@pytest.mark.parametrize("n_ts, sz, n_jobs", [(8, 100, 1)])
 def test_cdist_dtw_torch_cpu(benchmark, n_ts, sz, n_jobs):
+    # TODO: check threading on python free threading, not relevant otherwise
     X = torch.rand((n_ts, sz, 2))
     benchmark(cdist_dtw, X, X, n_jobs=n_jobs)
 
 
-@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1), (100, 1000, -1)])
+@pytest.mark.parametrize("n_ts, sz, n_jobs", [(7, 100, 1), (7, 1000, 1), (7, 1000, -1), (14, 1000, 1)])
 def test_dtw_barycenter(benchmark, n_ts, sz, n_jobs):
     rng = np.random.default_rng(0)
     X = rng.random((n_ts, sz, 2))
--- test_benchmark_soft_dtw.py
+++ test_benchmark_soft_dtw.py
@@ -10,8 +10,10 @@ from tslearn.metrics import SoftDTWLossPyTorch
 from tslearn.barycenters import softdtw_barycenter
 
 
-@pytest.mark.parametrize("sz", [100, 1000, 10000])
+@pytest.mark.parametrize("sz", [100, 1000, 2000, 4000, 8000])
 def test_soft_dtw_numpy(benchmark, sz):
+    # Mem: sz * 64 * 3 (inputs duplicated twice) + sz * sz * 8 (mask) + sz *sz * 64 (acc matrix)
+    # Care for swap, check runner mem
     X = np.random.rand(sz, 2)
     benchmark(soft_dtw, X, X)
 
@@ -25,7 +27,7 @@ def test_soft_dtw_torch_cpu(benchmark, sz):
     benchmark(soft_dtw, X, X, **kwargs)
 
 
-@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1), (10, 1000, 1), (10, 1000, -1)])
+@pytest.mark.parametrize("n_ts, sz, n_jobs", [(8, 100, 1), (8, 1000, 1), (8, 1000, -1)])
 def test_cdist_soft_dtw_numpy(benchmark, n_ts, sz, n_jobs):
     X = np.random.rand(n_ts, sz, 2)
     kwargs = {}
@@ -34,8 +36,9 @@ def test_cdist_soft_dtw_numpy(benchmark, n_ts, sz, n_jobs):
     benchmark(cdist_soft_dtw, X, X, **kwargs)
 
 
-@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1), (10, 100, -1)])
+@pytest.mark.parametrize("n_ts, sz, n_jobs", [(8, 100, 1)])
 def test_cdist_soft_dtw_torch_cpu(benchmark, n_ts, sz, n_jobs):
+    # TODO: check threading on python free threading, not relevant otherwise
     X = torch.rand((n_ts, sz, 2))
     kwargs = {}
     if 'compute_with_backend' in inspect.signature(soft_dtw).parameters:
@@ -45,7 +48,7 @@ def test_cdist_soft_dtw_torch_cpu(benchmark, n_ts, sz, n_jobs):
     benchmark(cdist_soft_dtw, X, X, **kwargs)
 
 
-@pytest.mark.parametrize("n_ts, sz, n_jobs", [(10, 100, 1), (10, 1000, -1)])
+@pytest.mark.parametrize("n_ts, sz, n_jobs", [(7, 100, 1), (7, 1000, 1), (7, 1000, -1), (14, 1000, 1)])
 def test_soft_dtw_barycenter(benchmark, n_ts, sz, n_jobs):
     rng = np.random.default_rng(0)
     X = rng.random((n_ts, sz, 2))
@@ -55,7 +58,7 @@ def test_soft_dtw_barycenter(benchmark, n_ts, sz, n_jobs):
         benchmark(softdtw_barycenter, X, max_iter=1)
 
 
-@pytest.mark.parametrize("n_ts, sz", [(10, 100), (10, 1000), (100, 1000)])
+@pytest.mark.parametrize("n_ts, sz", [(7, 100), (7, 1000)])
 def test_SoftDTWLossPyTorch(benchmark, n_ts, sz):
     X = torch.rand((n_ts, sz, 2))
     benchmark(SoftDTWLossPyTorch().forward, X, X)
